fad0f663bce6c0ecccc74917d96886a2499ad7f8
Sonny Yu <sonnyyuirm@gmail.com>
2022-03-06 16:56:40 -0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-if [ ! -f "/pki/openssl-1.0.cnf" ]; then
+if [ ! -f "/pki/openssl-easyrsa.cnf" ]; then
     echo "No OpensslConfig for EasyRsa Found. Copying the default"
     cp /usr/share/easy-rsa/openssl-easyrsa.cnf /pki/openssl-easyrsa.cnf
     cp -R /usr/share/easy-rsa/x509-types /pki/x509-types
