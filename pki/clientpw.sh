f94c044aa06c3934616151b77e0ff73fd37cee9f
sonny yu <sonnyyu123@gmail.com>
2022-03-25 05:16:03 +0000

    first commit

---
+++ pki/clientpw.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+export clientname="client2"
+export workdir="/home/sonnyyu/mtls-cert-manage/pki"
+sudo rm $workdir/pki/private/$clientname.key
+sudo rm $workdir/pki/issued/$clientname.crt
+sudo rm $workdir/pki/reqs/$clientname.req
+sudo easyrsa --passin=file:capassfile --passout=file:cppassfile  build-client-full $clientname 
+sudo easyrsa --passin=file:cppassfile --passout=file:p12passfile export-p12 $clientname
+#sudo rm -R $workdir/haproxycerts
+#mkdir  $workdir/haproxycerts
+sudo rm $workdir/clientcerts/*
+sudo cp $workdir/pki/private/$clientname.key  $workdir/clientcerts
+sudo cp $workdir/pki/private/$clientname.p12  $workdir/clientcerts
+sudo cp $workdir/pki/issued/$clientname.crt $workdir/clientcerts
+sudo cp $workdir/pki/ca.crt $workdir/clientcerts
+cd $workdir/clientcerts
+sudo chown sonnyyu:sonnyyu *
+sudo chmod 644  *
--- pki/haproxy.sh
+++ pki/haproxy.sh
@@ -6,19 +6,19 @@ sudo rm $workdir/pki/issued/$serverip.crt
 sudo rm $workdir/pki/reqs/$serverip.req
 sudo easyrsa --passin=file:capassfile --subject-alt-name="DNS:localhost,IP:$serverip" \
 build-server-full $serverip nopass
-#sudo rm -R $workdir/splunkcerts
-#mkdir  $workdir/splunkcerts
-sudo rm -R $workdir/splunkcerts/*
-sudo cp $workdir/pki/private/$serverip.key  $workdir/splunkcerts
-sudo cp $workdir/pki/issued/$serverip.crt $workdir/splunkcerts
-sudo cp $workdir/pki/ca.crt $workdir/splunkcerts
-cd $workdir/splunkcerts
+#sudo rm -R $workdir/haproxycerts
+#mkdir  $workdir/haproxycerts
+sudo rm $workdir/haproxycerts/*
+sudo cp $workdir/pki/private/$serverip.key  $workdir/haproxycerts
+sudo cp $workdir/pki/issued/$serverip.crt $workdir/haproxycerts
+sudo cp $workdir/pki/ca.crt $workdir/haproxycerts
+cd $workdir/haproxycerts
 sudo chown sonnyyu:sonnyyu *
 # add password into private key
 #openssl rsa -aes256 -in $serverip.key -out $serverip.pw.key
 # convert crt to pem
 openssl x509 -inform PEM -in $serverip.crt > $serverip.pem
 # genarate server.pem
-cat $serverip.pem $serverip.key ca.crt > server.pem
+cat $serverip.pem $serverip.key > server.pem
 sudo chown sonnyyu:sonnyyu *
 sudo chmod 644  *
